dc2cbf9111da57c8379308f30e3e09f7852a85d6
Shahil <shahilushah@gmail.com>
2020-06-07 13:53:45 -0400

    Changes to problem 21

--- Leet_Code/21_Merge_Two_Sorted_Lists.py
+++ Leet_Code/21_Merge_Two_Sorted_Lists.py
@@ -1,41 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        head = ListNode(-999999)
-        new_list = head
-        while(l1!=None and l2!=None):
-            if(l1.val>l2.val):
-                new_list.val = l2.val
-                l2 = l2.next
-            else:
-                new_list.val = l1.val
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        newList = ListNode()
+        head = newList
+        while(l1 != None and l2 != None):
+            if(l1.val < l2.val):
+                newList.next = l1
                 l1 = l1.next
-            new_list_node = ListNode(-999999)
-            new_list.next = new_list_node
-            new_list = new_list.next
-        if(l1==None):
-            while(l2!=None):
-                new_list.val = l2.val
-                new_list.next = ListNode(-999999)
-                new_list = new_list.next
+                newList = newList.next
+            else:
+                newList.next = l2
                 l2 = l2.next
-        elif(l2==None):
-            while(l1!=None):
-                new_list.val = l1.val
-                new_list.next = ListNode(-999999)
-                new_list = new_list.next
-                l1= l1.next
-        new_head = head
-        if(head.next == None):
-            return(l1)
-        while(head.next.val != -999999): 
-            head = head.next
-        head.next = None
-        return(new_head)
-        
-        
+                newList = newList.next
+        if(l1 == None):
+            newList.next = l2
+        else:
+            newList.next = l1
+        head = head.next
+        return(head)
